28888da001dacc256ee40ad1b936077f480f70fe
Joseph Emmanuel <joseph.emmanuel@outlook.com>
2022-11-02 15:01:52 -0500

    Added prefix

--- next.config.js
+++ next.config.js
@@ -5,7 +5,10 @@ const nextConfig = {
 
 // module.exports = nextConfig
 module.exports = {
+  basePath: '/static_generator',
+  assetPrefix: '/static_generator',
   trailingSlash: true,
+
   images: {
     loader: "imgix",
     path: "",
